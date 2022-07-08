62838aa0ca244a521edafb099ab2f4e63bb29523
samldr <sleader@ualberta.ca>
2022-07-07 21:22:41 -0600

    added fade-in on page load

---
+++ script.js
@@ -0,0 +1,7 @@
+function lightTheme() {
+    var element = document.body;
+    element.classList.add("light-mode"); }
+
+function darkTheme() {
+    var element = document.body;
+    element.classList.remove("light-mode"); }
