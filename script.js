66eaa13cd0b12068148a2189d8f442c6e1a03799
samldr <sleader@ualberta.ca>
2022-07-08 02:01:32 -0600

    added localStorage

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 function lightTheme() {
     var element = document.body;
-    element.classList.add("light-mode"); }
+    element.classList.remove('dark-mode');
+    element.classList.add('light-mode');
+    localStorage.setItem('mode','light-mode') 
+}
 
 function darkTheme() {
     var element = document.body;
-    element.classList.remove("light-mode"); }
+    element.classList.remove('light-mode');
+    element.classList.add('dark-mode'); 
+    localStorage.setItem('mode','dark-mode')
+}
+
+function themeCheck() {
+    if (localStorage.getItem('mode') != null){
+        var theme = localStorage.getItem('mode');
+        var element = document.body;
+        
+        if (element.classList.contains(theme) == false){
+            element.classList.add(theme);
+        }
+    } else {
+        darkTheme()
+    }
+    element.style.setProperty('visibility', 'visible');
+}
